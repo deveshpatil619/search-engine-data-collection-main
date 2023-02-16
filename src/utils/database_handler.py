f7a88745176b30613369b83f82b98b6c9f1e5b2a
DEVESH PATIL <deveshpatil619@gmail.com>
2023-02-16 14:37:49 +0530

    updates

--- src/utils/database_handler.py
+++ src/utils/database_handler.py
@@ -1,15 +1,25 @@
 import pymongo
 import os
+import urllib.parse
 
 
 class MongodbClient:
     client = None
 
     def __init__(self, database_name=os.environ["DATABASE_NAME"]) -> None:
+        username = urllib.parse.quote_plus(os.environ['ATLAS_CLUSTER_USERNAME'])
+        password = urllib.parse.quote_plus(os.environ['ATLAS_CLUSTER_PASSWORD'])
         if MongodbClient.client is None:
             MongodbClient.client = pymongo.MongoClient(
-                f"mongodb+srv://{os.environ['ATLAS_CLUSTER_USERNAME']}:{os.environ['ATLAS_CLUSTER_PASSWORD']}@projects.ch4mixt.mongodb.net/?retryWrites=true&w=majority"
+                f"mongodb+srv://{username}:{password}@cluster0.s8lbkcs.mongodb.net/?retryWrites=true&w=majority"
             )
         self.client = MongodbClient.client
         self.database = self.client[database_name]
         self.database_name = database_name
+
+        # Count the number of documents in the collection
+        collection = self.database["my_collection"]
+        print(collection.count_documents({}))
+
+
+
