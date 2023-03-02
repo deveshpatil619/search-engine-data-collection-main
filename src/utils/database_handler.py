5e57c78737056f3113403c0a13783abcc2441aa8
DEVESH PATIL <deveshpatil619@gmail.com>
2023-03-02 16:55:11 +0530

    updates

--- src/utils/database_handler.py
+++ src/utils/database_handler.py
@@ -1,25 +1,35 @@
+##The code imports pymongo library to interact with MongoDB database, os library to get environment 
+#variables, and urllib.parse to encode the username and password.
+
 import pymongo
 import os
 import urllib.parse
 
 
 class MongodbClient:
-    client = None
+    client = None ##  class variable client set to None
 
-    def __init__(self, database_name=os.environ["DATABASE_NAME"]) -> None:
+    def __init__(self, database_name=os.environ["DATABASE_NAME"]) -> None: 
+
+        """The code defines an initializer method for the MongodbClient class, which takes the
+        database_name as an argument, and it is set to the environment variable DATABASE_NAME.
+        The code encodes the username and password using urllib.parse.quote_plus() method."""
+
         username = urllib.parse.quote_plus(os.environ['ATLAS_CLUSTER_USERNAME'])
         password = urllib.parse.quote_plus(os.environ['ATLAS_CLUSTER_PASSWORD'])
-        if MongodbClient.client is None:
-            MongodbClient.client = pymongo.MongoClient(
+
+        if MongodbClient.client is None: #The code checks if the client is already created or not. 
+            MongodbClient.client = pymongo.MongoClient( ##If it's not created, it creates a new 
+#pymongo.MongoClient object by passing the MongoDB Atlas cluster URL, username, and password as arguments.
                 "mongodb://{}:{}@cluster0.s8lbkcs.mongodb.net/?retryWrites=true&w=majority".format(username,password)
             )
-        self.client = MongodbClient.client
-        self.database = self.client[database_name]
-        self.database_name = database_name
+        self.client = MongodbClient.client ##The code sets the client and database properties 
+        self.database = self.client[database_name] ## of the class to the client object and the 
+        self.database_name = database_name ##database_name passed to the initializer, respectively.
 
-        # Count the number of documents in the collection
-        collection = self.database["my_collection"]
-        print(collection.count_documents({}))
+       
+        collection = self.database["my_collection"] ##The code creates a new collection object by specifying the collection name my_collection
+        print(collection.count_documents({})) # Count the number of documents in the collection
 
 
 
--- src/utils/s3_handler.py
+++ src/utils/s3_handler.py
@@ -1,3 +1,10 @@
+"""os and sys are imported for handling operating system related tasks.
+boto3 is a Python library that provides low-level access to AWS services.
+boto3.session is imported for creating a session to connect to the S3 bucket.
+Dict is imported from typing for defining the return type of some methods.
+image_unique_name is a custom function from src.utils.utils that generates a unique name for an image.
+CustomException is a custom exception class from src.exception that formats error messages in a specific way."""
+
 import os, sys
 import boto3
 import boto3.session
@@ -8,16 +15,20 @@ from src.utils.utils import image_unique_name
 from src.exception import CustomException
 
 
-class S3Connection:
+class S3Connection: ## S3Connection is a class that provides methods to interact with the S3 bucket.
     """Data Class for reverse image search engine."""
 
-    def __init__(self):
-        session = boto3.Session(
+    def __init__(self): 
+        """The first line creates a new session object for boto3 with the AWS access key ID and 
+        AWS secret access key set in the environment variables AWS_ACCESS_KEY_ID and 
+        AWS_SECRET_ACCESS_KEY, respectively."""
+        session = boto3.Session( 
             aws_access_key_id=os.environ["AWS_ACCESS_KEY_ID"],
             aws_secret_access_key=os.environ["AWS_SECRET_ACCESS_KEY"],
         )
-        self.s3 = session.resource("s3")
-        self.bucket = self.s3.Bucket(os.environ["AWS_BUCKET_NAME"])
+        self.s3 = session.resource("s3") # creates an S3 resource object using the session object.
+        self.bucket = self.s3.Bucket(os.environ["AWS_BUCKET_NAME"]) # sets the bucket attribute of the class 
+# instance to the S3 bucket specified in the environment variable AWS_BUCKET_NAME
 
     def add_label(self, label: str) -> Dict:
         """
@@ -26,11 +37,11 @@ class S3Connection:
          :return: json Response of state message (success or failure)
          """
         try:
-            key = f"images/{label}/"
-            response = self.bucket.put_object(Body="", Key=key)
-            return {"Created": True, "Path": response.key}
+            key = f"images/{label}/" ##key is the S3 object key that specifies the path of the new folder in the S3 bucket.
+            response = self.bucket.put_object(Body="", Key=key) #self.bucket.put_object is used to create the new folder in the S3 bucket.
+            return {"Created": True, "Path": response.key} ## response.key returns the path of the new folder.
         except Exception as e:
-            message = CustomException(e, sys)
+            message = CustomException(e, sys) ##If an exception occurs, CustomException is used to format the error message and return it in a dictionary format.
             return {"Created": False, "Reason": message.error_message}
 
     def upload_to_s3(self, image_path, label: str):
