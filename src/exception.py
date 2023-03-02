860afbee08cdc1af3ed13be48b2123ed17904bbe
DEVESH PATIL <deveshpatil619@gmail.com>
2023-03-02 19:22:30 +0530

    updates

--- src/exception.py
+++ src/exception.py
@@ -6,10 +6,20 @@ class IntegrityError(Exception):
 
 
 def error_message_detail(error, error_detail):
-    _, _, exc_tb = error_detail.exc_info()
-    file_name = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+
+#The purpose of this function is to format error messages that include the file name, line number, and 
+#error message. 
+
+    _, _, exc_tb = error_detail.exc_info() #This line extracts information about the current exception using the
+#exc_info() method of the error_detail object. The method returns a tuple containing the exception type, value,
+#  and traceback. We only need the traceback, so we use the underscore (_) to indicate that we are not interested in the first two elements of the tuple.
+    file_name = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1] #This line extracts the file name where the
+#error occurred from the traceback object using the os.path.split() method. The method splits the file path into 
+# a directory and file name, and we only need the file name, so we use [1] to access it.
     error_message = "Error occurred python script name [{0}] line number [{1}] error message [{2}]".format(
-        file_name, exc_tb.tb_lineno, str(error)
+        file_name, exc_tb.tb_lineno, str(error) ## his line formats the error message as a string using the 
+#format() method. The string includes placeholders {0}, {1}, and {2} that are replaced with the values of 
+# file_name, exc_tb.tb_lineno, and str(error) respectively.
     )
 
     return error_message
--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,6 +1,16 @@
 import uuid
+"""This Python function generates a unique name for an image by using the UUID (Universally Unique Identifier)
+ library. The function uses the uuid.uuid1() method to generate a UUID version 1, which is based on the current
+time, the computer's MAC address, and a random number."""
 
 
 # Generate Unique Names for all the images
 def image_unique_name():
-    return "img-" + str(uuid.uuid1())
+    return "img-" + str(uuid.uuid1()) ## The UUID is then converted to a string and concatenated with the 
+#prefix "img-" to form the unique image name. The resulting name is a string in the format "img-<uuid>", 
+# where <uuid> is a unique identifier.
+
+
+
+
+
